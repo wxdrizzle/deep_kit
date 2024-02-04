d8e7729d19eca4283d26125d1b011d69d46d7c8a
Xin Wang <xinwang.git@outlook.com>
2024-02-03 20:09:54 -0800

    send cfg to clearML as hyperparameters

--- src/deep_kit/cfgs/collect_cfg.py
+++ src/deep_kit/cfgs/collect_cfg.py
@@ -1,3 +1,5 @@
+from clearml import Task
+
 from omegaconf import OmegaConf as Ocfg
 import importlib.resources
 from .. import cfgs
@@ -10,19 +12,16 @@ cfg_cli = Ocfg.from_cli()
 
 prefix = cfg_cli.exp.name[:2]
 cfg_cli.exp.name = cfg_cli.exp.name[2:]
-
 if prefix == 'tr':
     cfg_exp = Ocfg.load(f'cfgs/{cfg_cli.exp.name}.yml')
 elif prefix == 'te':
     cfg_exp = Ocfg.load(f'cfgs/test/{cfg_cli.exp.name}.yml')
 else:
     raise ValueError
-
 if 'name' in cfg_exp.exp and cfg_exp.exp.name != cfg_cli.exp.name:
     raise ValueError(
         f'The yaml file name and the "exp.name" in the yaml must be the same, but got "{cfg_cli.exp.name}" and "{cfg_exp.exp.name}", respectively.'
     )
-
 cfg_default_model = Ocfg.load(f'cfgs/default/models/{cfg_exp.model.name}.yml')
 cfg_default_dataset = Ocfg.load(f'cfgs/default/datasets/{cfg_exp.dataset.name}.yml')
 
@@ -38,3 +37,29 @@ cfg.exp.train.scheduler = Ocfg.masked_copy(cfg.exp.train.scheduler, keys_masked_
 
 Ocfg.set_readonly(cfg, True)
 Ocfg.set_readonly(cfg.var, False)
+
+if cfg.exp.mode == 'train':
+    task_type = 'training'
+elif cfg.exp.mode == 'test':
+    task_type = 'testing'
+else:
+    raise ValueError
+task = Task.init(project_name=cfg._meta.project, task_name=cfg.exp.name, task_type=task_type)
+
+
+def flatten_dict(nested_dict, parent_key='', sep='.'):
+    flat_dict = {}
+    for key, value in nested_dict.items():
+        if key in ['var', '_meta']:
+            continue
+        new_key = f"{parent_key}{sep}{key}" if parent_key else key
+        if isinstance(value, dict):
+            flat_dict.update(flatten_dict(value, new_key))
+        else:
+            flat_dict[new_key] = value
+    return flat_dict
+
+
+dict_cfg = flatten_dict(Ocfg.to_container(cfg))
+dict_cfg = {k.replace('.', '/', 1): v for k, v in dict_cfg.items()}
+task.set_parameters(dict_cfg)
